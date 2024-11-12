b2ea2a8abd7b46b4e0142801ae14c0f72d2f3f68
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-11-12 18:20:15 +0100

    eventBus + vue router

---
+++ src/router.js
@@ -0,0 +1,87 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AppHome from "./views/AppHome.vue";
+import AppContact from "./views/AppContact.vue";
+import AppAbout from "./views/AppAbout.vue";
+import MangaShow from "./views/MangaShow.vue";
+import AppTerms from "./views/AppTerms.vue";
+import AppCookies from"./views/AppCookies.vue";
+import AppPrivacy from "./views/AppPrivacy.vue";
+import AppFaq from "./views/AppFAQ.vue";
+import Cart from "./components/Cart.vue";
+import Checkout from "./components/checkout.vue";
+import AppThanksOrder from './Views/AppThanksOrder.vue';
+import Not_found from "./views/Not_found.vue";
+
+
+
+
+
+// Importa i tuoi componenti
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "AppHome",
+      component: AppHome,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AppAbout,
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: AppContact,
+    },
+    {
+      path: "/manga/:slug", //add slug
+      name: "MangaShow",
+      component: MangaShow,
+    },
+    {
+      path: "/terms",
+      name: "terms",
+      component: AppTerms,
+    },
+    {
+      path: "/privacy",
+      name: "privacy",
+      component: AppPrivacy,
+    },
+    {
+      path: "/cookies",
+      name: "cookies",
+      component: AppCookies,
+    },
+    {
+      path: "/FAQ",
+      name: "FAQ",
+      component: AppFaq,
+    },
+    {
+      path: "/cart",
+      name: "cart",
+      component: Cart,
+    },
+    {
+      path: "/checkout",
+      name: "checkout",
+      component: Checkout,
+    },
+    {
+      path: "/thanks",
+      name: "thanks",
+      component: AppThanksOrder,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not_found",
+      component: Not_found,
+    },
+  ],
+});
+
+export { router };
