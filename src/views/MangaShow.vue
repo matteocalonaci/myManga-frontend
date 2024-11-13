c90388b8bf83a5d80052e671c03c7950d9957c16
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-11-13 18:14:16 +0100

    manga_show

--- src/views/MangaShow.vue
+++ src/views/MangaShow.vue
@@ -27,27 +27,41 @@ export default {
             axios.get(url)
                 .then(response => {
                     console.log("Risposta dell'API:", response.data);
-                    this.manga = response.data.manga; 
+                    this.manga = response.data.manga;
                 })
                 .catch(error => {
                     console.error("Si è verificato un errore durante il recupero del manga:", error);
                 })
                 .finally(() => {
-                    this.loading = false; 
+                    this.loading = false;
                 });
         },
         toggleLike() {
-            this.isLiked = !this.isLiked; 
+            this.isLiked = !this.isLiked;
         },
+
+        increaseQuantity() {
+            this.quantity++;
+        },
+        decreaseQuantity() {
+            if (this.quantity > 1) {
+                this.quantity--;
+            }
+        },
+        addToCart() {
+            // Logica per aggiungere al carrello
+            console.log(`Aggiunto ${this.quantity} articoli al carrello.`);
+        }
+
     },
     computed: {
         iconStyle() {
             return {
                 backgroundColor: 'black',
-                borderRadius: '50%', 
-                padding: '0.5rem', 
-                display: 'inline-block', 
-                color: this.isLiked ? 'red' : 'black', 
+                borderRadius: '50%',
+                padding: '0.5rem',
+                display: 'inline-block',
+                color: this.isLiked ? 'red' : 'black',
             };
         },
     },
@@ -57,54 +71,74 @@ export default {
     <LoadingScreen v-if="loading" />
 
     <div v-else-if="manga">
-        <div class="col-12 p-5 d-flex justify-content-between">
-            <div class="col-5">
-                <div class="img-container" :style="{ borderRadius: '10px 10px 0 0' }">
-                    <img :src="manga.cover_image" class="card-img-top img-fluid" alt=""
-                        style="object-fit: cover; border-radius: 10px 10px 0 0;">
-                    <i :class="['fa-heart text-danger', isLiked ? 'fa-solid' : 'fa-regular']" @click.stop="toggleLike"
-                        :style="iconStyle" style="cursor: pointer;"></i>
+        <div class="container p-5">
+            <div class="row">
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="img-container">
+                        <img :src="manga.cover_image" class="card-img-top img-fluid" alt=""
+                            style="object-fit: cover; border-radius: 1rem;">
+                        <i :class="['fa-heart text-danger', isLiked ? 'fa-solid' : 'fa-regular']"
+                            @click.stop="toggleLike" :style="iconStyle" style="cursor: pointer;"></i>
+                    </div>
+                </div>
+                <div class="col-12 col-md-8">
+                    <div class="card-body">
+                        <h2 class="card-title"><b>{{ manga.title }}</b></h2>
+                        <h5 class="card-text mt-3 mb-4"><b>€{{ manga.price }}</b></h5>
+                        <p class="card-text">
+                            <span class="th">Generi:</span>
+                            <span class="genres-container d-flex flex-wrap">
+                                <span class="genre" v-for="genre in manga.genres" :key="genre.id">{{ genre.name
+                                    }}</span>
+                            </span>
+                        </p>
+                        <p class="card-text"><span class="th">Autore:</span> {{ manga.authors.name }}</p>
+                        <p class="card-text"><span class="th">Volume:</span> {{ manga.volume }}</p>
+                        <p class="card-text"><span class="th">Disponibilità:</span> {{ manga.availability ?
+                            'Disponibile' : 'Non disponibile' }}</p>
+                        <p class="card-text"><span class="th">In corso? </span> {{ manga.in_progress ? 'Sì' : 'No' }}
+                        </p>
+                        <p class="card-text"><span class="th">Anno: </span>{{ manga.year }}</p>
+                        <p class="card-text"><span class="th">Categoria: </span> {{ manga.categories.name }}</p>
+                        <p class="card-text"><span class="th">Editore: </span> {{ manga.editors.name }}</p>
+
+                        <div class="d-flex flex-column flex-md-row justify-content-between align-items-center mt-3">
+                            <div class="quantiy-btn d-flex align-items-center mb-2 mb-md-0">
+                                <button @click="decreaseQuantity" class="pulsanti">-</button>
+                                <span class="quantity mx-2">1</span>
+                                <button @click="increaseQuantity" class="pulsanti">+</button>
+                            </div>
+
+                            <button class="addToCart flex-grow-1">Aggiungi al carrello <i
+                                    class="fa-solid fa-cart-shopping"></i></button>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-            <div class="col-7 px-5">
-                <div class="card-body">
-    <h2 class="card-title"><b>{{ manga.title }}</b></h2>
-    <h5 class="card-text mt-3 mb-4"><b>€{{ manga.price }}</b></h5>
-    <p class="card-text"><span class="th">Generi:</span> 
-        <span class="genre" v-for="genre in manga.genres" :key="genre.id"> {{ genre.name }}</span>
-    </p>
-    <p class="card-text"><span class="th">Autore:</span> {{ manga.authors.name }}</p>
-    <p class="card-text"><span class="th">Volume:</span> {{ manga.volume }}</p>
-    <p class="card-text"><span class="th">Disponibilità:</span> {{ manga.availability ? 'Disponibile' : 'Non disponibile' }}</p>
-    <p class="card-text"><span class="th">In corso? </span> {{ manga.in_progress ? 'Sì' : 'No' }}</p>
-    <p class="card-text"><span class="th">Anno di Pubblicazione: </span>{{ manga.year }}</p>
-    <p class="card-text"><span class="th">Categoria: </span> {{ manga.categories.name }}</p>
-    <p class="card-text"><span class="th">Editore: </span> {{ manga.editors.name }}</p>
-    <p class="card-text">{{ manga.description }}</p>
-    <div class="text-center">
-        <button class="addToCart mt-2">Aggiungi al carrello</button>
-    </div>
-</div>
 
+            <div class="description-container mt-4">
+                <p class="card-text"><span class="th">Descrizione:</span></p>
+                <hr>
+                <p class="card-text">{{ manga.description }}</p>
             </div>
         </div>
     </div>
+
     <div v-else>
         <p>Manga non trovato.</p>
     </div>
 </template>
 
-
 <style scoped>
-
 .genre {
-  margin: 0.5rem;
-  padding: 0.5rem;
-  color: white;
-  background-color: rgb(250, 0, 83);
-
-  border-radius: 2rem;
+    margin: 0.5rem;
+    padding: 0.5rem;
+    color: white;
+    background-color: rgb(250, 0, 83);
+    border-radius: 2rem;
 }
+
 .fa-heart {
     position: absolute;
     top: 0.5rem;
@@ -113,28 +147,73 @@ export default {
     transition: color 0.3s ease;
 }
 
-.addToCart {
-    background-color: rgb(250, 0, 83);
-    color: white;
-    border-radius: 2rem;
-    padding: 0.5rem 1rem;
-    border: none;
-}
-
-.addToCart:hover {
-    background-color: black;
-    color: white;
-}
-
-.card-body{
+.card-body {
     background-color: white;
     padding: 1rem;
-    border-radius: 2rem
+    border-radius: 1rem;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.377);
+    /* Aggiunta dell'ombra */
 }
 
-.th{
-    color:rgb(250, 0, 83);
+.th {
+    color: rgb(250, 0, 83);
     font-weight: bold;
 }
 
+.description-container {
+    padding: 1rem;
+    border-radius: 2rem;
+    background-color: white;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.377);
+    /* Aggiunta dell'ombra */
+}
+
+.img-container {
+    max-height: 32.5rem;
+    border-radius: 1rem;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.377);
+    /* Aggiunta dell'ombra */
+}
+
+.quantiy-btn {
+    padding: 1rem 2rem;
+    border-radius: 2rem;
+    color:  rgb(250, 0, 83);
+    border: 0.1rem solid rgb(250, 0, 83);
+}
+
+.quantity {
+    padding: 0 3rem;
+}
+
+.pulsanti {
+    border: none;
+    color: rgb(250, 0, 83);
+    background-color: white;
+    font-size: 1rem;
+}
+
+.quantiy-btn:hover {
+    background-color: black;
+    color: white;
+    border: 0.1rem solid black;
+}
+
+.quantiy-btn:hover .pulsanti {
+    color: white;
+    background-color: black;
+}
+
+.addToCart {
+    padding: 1rem 2rem;
+    background-color: rgb(250, 0, 83);
+    border-radius: 2rem;
+    border: none;
+    color: white;
+}
+
+.addToCart:hover {
+    color: white;
+    background-color: black;
+}
 </style>
