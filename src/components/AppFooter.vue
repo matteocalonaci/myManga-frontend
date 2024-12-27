c40d0f757960c3418e9882ddbcf6ff875446a8d4
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-12-27 16:08:48 +0100

    fix footer mobile e ricerca manga

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,30 @@
 <script>
 export default {
     name: "AppFooter",
-    components: {},
+    data() {
+        return {
+            isOpen: {
+                prodotti: false,
+                noteLegali: false,
+                help: false
+            },
+            isDesktop: window.innerWidth > 768
+        };
+    },
+    methods: {
+        toggleText(section) {
+            this.isOpen[section] = !this.isOpen[section];
+        },
+        checkWindowSize() {
+            this.isDesktop = window.innerWidth > 768;
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.checkWindowSize);
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.checkWindowSize);
+    }
 }
 </script>
 
@@ -16,45 +39,80 @@ export default {
                 <div class="col-12">
 
                     <div class="text-white d-flex flex-column flex-md-row justify-content-around p-4">
-                        <div class="d-flex flex-column p-2 text-center">
-                            <span>MYMANGA</span>
+                        <div class="d-flex flex-column pl-2 text-center">
+                            <h3>MYMANGA</h3>
                             <hr>
                             <div class="img-container">
                                 <img src="../assets/img/kana_logo.jpeg" alt="">
                             </div>
                         </div>
 
-                        <div class="d-flex flex-column p-2 text-center">
-                            <span>PRODOTTI</span>
-                            <hr>
-                            <router-link to="/about">Chi siamo</router-link>
-                            <router-link to="/manga">Manga</router-link>
-                        </div>
+                        <div class="footer-link">
+                            <div class="mb-5 d-flex">
+                                <div class="col-11">
+                                    <h6 class="faq-text mb">PRODOTTI</h6>
+                                    <div class="toggle-content" v-show="isOpen.prodotti || isDesktop">
+                                        <!-- Mostra solo se aperto o in desktop -->
+                                        <hr />
+                                        <div class="row d-flex flex-column" style="min-width: 5rem;">
+                                            <router-link to="/about">Chi siamo</router-link>
+                                            <router-link to="/manga">Manga</router-link>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-1">
+                                    <span class="toggle-button" @click="toggleText('prodotti')"
+                                        style="font-size: 1.5rem">{{ isOpen.prodotti ? '-' : '+' }}</span>
+                                </div>
+                            </div>
 
-                        <div class="d-flex flex-column p-2 text-center">
-                            <span>NOTE LEGALI</span>
-                            <hr>
-                            <router-link to="/terms">Termini e condizioni</router-link>
-                            <router-link to="/privacy">Informativa sulla privacy</router-link>
-                            <router-link to="/cookies">Cookies</router-link>
-                        </div>
+                            <div class="mb-5 d-flex">
+                                <div class="col-11">
+                                    <h6 class="faq-text mb">NOTE LEGALI</h6>
+                                    <div class="toggle-content" v-show="isOpen.noteLegali || isDesktop">
+                                        <!-- Mostra solo se aperto o in desktop -->
+                                        <hr />
+                                        <div class="row d-flex flex-column" style="min-width: 5rem;">
+                                            <router-link to="/terms">Termini e condizioni</router-link>
+                                            <router-link to="/privacy">Informativa sulla privacy</router-link>
+                                            <router-link to="/cookies">Cookies</router-link>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-1">
+                                    <span class="toggle-button" @click="toggleText('noteLegali')"
+                                        style="font-size: 1.5rem">{{ isOpen.noteLegali ? '-' : '+' }}</span>
+                                </div>
+                            </div>
 
-                        <div class="d-flex flex-column p-2 text-center">
-                            <span>HELP</span>
-                            <hr>
-                            <router-link to="/FAQ">FAQ</router-link>
-                            <router-link to="/contact">Contact</router-link>
+                            <div class="mb-5 d-flex">
+                                <div class="col-11">
+                                    <h6 class="faq-text mb">HELP</h6>
+                                    <div class="toggle-content" v-show="isOpen.help || isDesktop">
+                                        <!-- Mostra solo se aperto o in desktop -->
+                                        <hr />
+                                        <div class="row d-flex flex-column" style="min-width: 5rem;">
+                                            <router-link to="/FAQ">FAQ</router-link>
+                                            <router-link to="/contact">Contact</router-link>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-1">
+                                    <span class="toggle-button" @click="toggleText('help')" style="font-size: 1.5rem">{{
+                                        isOpen.help ? '-' : '+' }}</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
                 </div>
             </div>
             <div class="row d-flex justify-content-between align-items-center" style="height: 3.5rem;">
-                <div class="col-6" style="color: rgba(0, 0, 0, 0.6);"><b>©2024 myManga s.r.l. P.IVA: xxxxxxxxxxx</b></div>
-                
-                    <div class="col-6 text-end"><img src="https://pngimg.com/d/mastercard_PNG27.png" alt="" style="height: 2rem;"></div>
-                
+                <div class="col-6" style="color: rgba(0, 0, 0, 0.6);"><b>©2024 myManga s.r.l. P.IVA: xxxxxxxxxxx</b>
+                </div>
 
+                <div class="col-6 text-end"><img src="https://pngimg.com/d/mastercard_PNG27.png" alt=""
+                        style="height: 2rem;"></div>
             </div>
         </div>
     </footer>
@@ -62,9 +120,15 @@ export default {
 
 <style scoped>
 a {
-    color: white;
+    color: rgba(255, 255, 255, 0.605);
     text-decoration: none;
     transition: font-size 0.2s;
+}
+
+.footer-link {
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
 }
 
 a:hover {
@@ -72,7 +136,7 @@ a:hover {
 }
 
 .img-container {
-    width: 100%; 
+    width: 100%;
     max-width: 10rem;
 }
 
@@ -81,15 +145,65 @@ a:hover {
     border-radius: 10rem;
 }
 
-/* Media Queries per schermi più piccoli */
+.mb-5 {
+    margin-bottom: 1.5rem;
+}
+
+.faq-text {
+    cursor: pointer;
+}
+
+.toggle-content {
+    display: flex;
+    flex-direction: column;
+}
+
+.toggle-button {
+    cursor: pointer;
+    font-size: 1.5rem;
+    display: none;
+    /* Nascondi il pulsante in desktop */
+}
+
 @media (max-width: 768px) {
     .text-white {
-        flex-direction: column; 
-        align-items: center; 
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
     }
 
     .img-container {
         margin-bottom: 1rem;
+        width: 100%;
+        max-width: 10rem;
+    }
+
+    .faq-text {
+        margin: 0;
+    }
+
+    .toggle-button {
+        display: block;
+        /* Mostra il pulsante in mobile */
+    }
+
+    .footer-link {
+        margin-top: 3rem;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    .mb-5 {
+        justify-content: center;
+        align-items: center;
+    }
+
+    .toggle-content {
+        display: block;
+        text-align: center;
     }
 }
 </style>
--- src/components/MangaList.vue
+++ src/components/MangaList.vue
@@ -1,32 +1,35 @@
 <template>
     <div>
         <LoadingScreen v-if="isLoading" /> <!-- Mostra il caricamento se isLoading è true -->
-        <div v-if="!isLoading && filteredMangas.length > 0">
+        <div v-if="!isLoading">
             <div class="container" style="background-color: rgb(250, 0, 83);">
                 <div class="row mt-5">
                     <div class="col-12">
                         <div class="search-filters d-flex justify-content-center">
                             <input type="text" v-model="filterQuery" placeholder="Cerca per titolo..."
-                            @input="applyFilters" />
+                                @input="applyFilters" />
                             <select v-model="selectedAuthor" @change="applyFilters">
                                 <option value="">Seleziona Autore</option>
-                                <option v-for="author in authors" :key="author.id" :value="author.id">{{ author.name }}</option>
+                                <option v-for="author in authors" :key="author.id" :value="author.id">{{ author.name }}
+                                </option>
                             </select>
                             <select v-model="selectedGenre" @change="applyFilters">
                                 <option value="">Seleziona Genere</option>
-                                <option v-for="genre in genres" :key="genre.id" :value="genre.id">{{ genre.name }}</option>
+                                <option v-for="genre in genres" :key="genre.id" :value="genre.id">{{ genre.name }}
+                                </option>
                             </select>
                             <select v-model="selectedEditors" @change="applyFilters">
                                 <option value="">Seleziona Editore</option>
-                                <option v-for="editor in editors" :key="editor.id" :value="editor.id">{{ editor.name }}</option>
+                                <option v-for="editor in editors" :key="editor.id" :value="editor.id">{{ editor.name }}
+                                </option>
                             </select>
                         </div>
                         <hr>
                         <div class="card-container d-flex justify-content-center align-items-center">
-                            <div v-if="paginatedMangas.length === 0" class="">
-                                <div class="empty-card-container position-relative d-flex justify-content-center align-items-center">
-                                    <img src="../assets/img/sad-kana.webp" alt="sad-kana" style="width: 40rem;">
-                                    <p class="text position-absolute bottom-0"><b>NESSUN RISULTATO TROVATO</b></p>
+                            <div v-if="filteredMangas.length === 0" class="empty-card-container">
+                                <p class="text"><b>NESSUN RISULTATO TROVATO</b></p>
+                                <div class="img-container">
+                                    <img src="../assets/img/sad-kana.webp" alt="">
                                 </div>
                             </div>
                             <div v-for="manga in paginatedMangas" :key="manga.id" class="card-wrapper">
@@ -37,12 +40,10 @@
                     </div>
                 </div>
             </div>
-            <div class="pagination">
+            <div class="pagination" v-if="totalPages > 1">
                 <div class="controller-buttons">
-                    <button v-for="page in totalPages" 
-                            :key="page" 
-                            @click="changePage(page)" 
-                            :class="{ active: currentPage === page }">
+                    <button v-for="page in totalPages" :key="page" @click="changePage(page)"
+                        :class="{ active: currentPage === page }">
                         {{ page }}
                     </button>
                 </div>
@@ -63,7 +64,7 @@ export default {
         Single_Manga
     },
     data() {
- return {
+        return {
             base_url: 'http://localhost:8000/',
             mangas: [],
             isLoading: false,
@@ -90,50 +91,50 @@ export default {
     },
     methods: {
         loadMangas() {
-            this.isLoading = true;
-            const url = `${this.base_url}api/mangas`;
-            axios.get(url, {
-                params: {
-                    per_page: 100
-                }
-            })
-            .then(response => {
-                this.mangas = response.data.mangas.data;
-                this.filteredMangas = this.mangas;
-                this.totalPages = Math.ceil(this.filteredMangas.length / this.mangasPerPage); // Calcola il numero totale di pagine
-                this.isLoading = false;
-            })
-            .catch(error => {
-                console.error("Errore nel caricamento dei manga:", error);
-                this.isLoading = false;
-            });
-        },
-        applyFilters() {
-            this.filteredMangas = this.mangas.filter(manga => {
-                return (
-                    (this.filterQuery === '' || manga.title.includes(this.filterQuery)) &&
-                    (this.selectedAuthor === '' || manga.author_id === this.selectedAuthor) &&
-                    (this.selectedGenre === '' || manga.genre_id === this.selectedGenre) &&
-                    (this.selectedEditors === '' || manga.editor_id === this.selectedEditors)
-                );
-            });
-            this.totalPages = Math.ceil(this.filteredMangas.length / this.mangasPerPage); // Ricalcola le pagine dopo il filtraggio
-            this.currentPage = 1; // Resetta alla prima pagina dopo il filtraggio
-        },
-        changePage(page) {
-            this.currentPage = page; // Cambia la pagina corrente
-        },
-        toggleWishList(manga) {
-            const index = this.wishList.indexOf(manga.id);
-            if (index === -1) {
-                this.wishList.push(manga.id);
-            } else {
-                this.wishList.splice(index, 1);
-            }
+    this.isLoading = true;
+    const url = `${this.base_url}api/mangas`;
+    axios.get(url, {
+        params: {
+            per_page: 100
         }
+    })
+    .then(response => {
+        console.log(response.data); // Controlla la risposta
+        this.mangas = response.data.mangas.data;
+        this.filteredMangas = this.mangas;
+        this.totalPages = Math.ceil(this.filteredMangas.length / this.mangasPerPage);
+
+        // Assegna i dati per autori, generi, categorie ed editori
+        this.authors = response.data.authors;
+        this.genres = response.data.genres;
+        this.categories = response.data.categories;
+        this.editors = response.data.editors;
+
+        this.isLoading = false;
+    })
+    .catch(error => {
+        console.error("Errore nel caricamento dei manga:", error);
+        this.isLoading = false;
+    });
+},
+applyFilters() {
+    this.filteredMangas = this.mangas.filter(manga => {
+        const genreMatch = this.selectedGenre === '' || manga.genres.some(genre => genre.id === this.selectedGenre);
+        return (
+            (this.filterQuery === '' || manga.title.toLowerCase().includes(this.filterQuery.toLowerCase())) &&
+            (this.selectedAuthor === '' || manga.author_id === this.selectedAuthor) &&
+            genreMatch && // Controlla se il manga ha il genere selezionato
+            (this.selectedEditors === '' || manga.editor_id === this.selectedEditors)
+        );
+    });
+    this.totalPages = Math.ceil(this.filteredMangas.length / this.mangasPerPage); // Ricalcola le pagine dopo il filtraggio
+    this.currentPage = 1; // Resetta alla prima pagina dopo il filtraggio
+}
     },
     mounted() {
         this.loadMangas(); // Carica i manga quando il componente è montato
+        console.log(this.authors);
+
     }
 }
 </script>
@@ -186,15 +187,29 @@ export default {
     color: rgb(250, 0, 83);
 }
 
-.text {
-    padding: 0 0.5rem;
-    background-color: black;
-    color: white;
-    font-size: 2rem;
-}
 
 .empty-card-container {
     margin: 2rem;
+    text-align: center;
+    /* Centra il testo */
+}
+
+.img-container{
+    width:100%; 
+  height: auto; 
+  object-fit: cover; 
+  transition: transform 0.3s;
+}
+img{
+    width: 100%;
+}
+
+
+.text {
+    height: 100%;
+    padding: 0 0.5rem;
+    color: rgba(0, 0, 0, 0.544);
+    font-size: 2rem;
 }
 
 @media (max-width: 768px) {
@@ -212,11 +227,11 @@ export default {
         margin: 2rem;
     }
 
-    .text {
-        padding: 0 0.5rem;
-        background-color: black;
-        color: white;
-        font-size: 1.5rem;
-    }
+.text {
+    height: 100%;
+    padding: 0 0.5rem;
+    color: rgba(0, 0, 0, 0.544);
+    font-size: 1.5rem;
+}
 }
 </style>
